885790ebd50362a770bdbadb763a31a3a266d903
Mizi00 <toufikboukichou.sio@gmail.com>
2025-11-14 14:24:16 +0100

    feat(endpoints): add user-related routes for fetching user details and interactions

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,11 +1,17 @@
 <?php
 
-use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 
 Route::post('login', [AuthController::class, 'login']);
 Route::post('register', [AuthController::class, 'register']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::delete('/logout', [AuthController::class, 'logout']);
+
+    Route::get('/user/{id}', [UserController::class, 'show']);
+    Route::get('/user/{id}/videos', [UserController::class, 'videos']);
+    Route::get('/user/{id}/liked-videos', [UserController::class, 'likedVideos']);
+    Route::get('/user/{id}/liked-comments', [UserController::class, 'likedComments']);
 });
