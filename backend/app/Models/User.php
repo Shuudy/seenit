761cd7729b398784b413fddff1891ad76bcd18f1
Mizi00 <toufikboukichou.sio@gmail.com>
2025-11-14 16:46:09 +0100

    fix(User): simplify likedVideos relationship definition

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -60,7 +60,7 @@ class User extends Authenticatable
      */
     public function likedVideos(): BelongsToMany
     {
-        return $this->belongsToMany(Video::class, 'user_video', 'user_id', 'video_id');
+        return $this->belongsToMany(Video::class, 'user_video');
     }
 
     /**
