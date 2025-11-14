8465be184bbfe6761d29abde3632f1b3fb5e3560
Shuudy <ancieux.jerome059@gmail.com>
2025-11-14 16:07:14 +0100

    feat(routes): add fallback route for 404 error handling

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Resources\ErrorResource;
 use Illuminate\Support\Facades\Route;
 
 Route::post('login', [AuthController::class, 'login']);
@@ -8,4 +9,11 @@ Route::post('register', [AuthController::class, 'register']);
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::delete('/logout', [AuthController::class, 'logout']);
+});
+
+Route::fallback(function () {
+    return new ErrorResource([
+        'message' => 'Endpoint not found.',
+        'status_code' => 404,
+    ]);
 });
