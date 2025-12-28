02ee32fcbf4962fedcfd08a8a284b456dadc1a04
Maxcastel <maxence.nord@gmail.com>
2025-12-28 23:00:25 +0100

    feat(getAvatarUrl): one letter instead of two

--- frontend/src/utils/get-avatar-url.ts
+++ frontend/src/utils/get-avatar-url.ts
@@ -1,6 +1,6 @@
 export function getAvatarUrl(username: string, avatarUrl?: string | null): string {
   return (
     avatarUrl ??
-    `https://ui-avatars.com/api/?name=${encodeURIComponent(username)}&background=random&color=fff`
+    `https://ui-avatars.com/api/?name=${encodeURIComponent(username)}&background=random&color=fff&length=1`
   );
 }
